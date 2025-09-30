a34fa77880cc2c80f0af06215c89fdb1dd7ecdca
jsongo <jsongo@qq.com>
2025-09-30 11:28:47 +0800

    feat: add fe codes

---
+++ frontend/eslint.config.js
@@ -0,0 +1,50 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import reactRefresh from "eslint-plugin-react-refresh";
+
+export default tseslint.config(
+  { ignores: ["dist", "node_modules", "server/", "**/*.config.js", "**/*.config.ts"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,tsx}"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: [
+          "./tsconfig.json",
+          "./tsconfig.node.json"
+        ],
+      },
+      globals: globals.browser,
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    plugins: {
+      react: pluginReact,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...pluginReact.configs.recommended.rules,
+      ...pluginReact.configs["jsx-runtime"].rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "react/jsx-no-target-blank": "off",
+      "react/react-in-jsx-scope": "off",
+    },
+  }
+);
